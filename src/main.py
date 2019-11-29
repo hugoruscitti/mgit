48ca228347200f2873cd9007e8f583d61a7a6f13
Hugo Ruscitti <hugoruscitti@gmail.com>
2019-11-29 20:16:28 -0300

    Eliminando uso de yaspin y mejorando la barra de progreso.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 from colorama import Fore, Back, Style
 from tabulate import tabulate
-from yaspin import yaspin
-from yaspin.spinners import Spinners
 import subprocess
 import time
 import math
@@ -15,6 +13,10 @@ path = sys.argv[1]
 LIMITE = 30
 CANTIDAD_DE_PROCESOS_EN_PARALELO = 10
 
+gris = '\033[90m'
+verde = '\033[92m'
+fin = '\033[0m'
+
 
 def obtener_grupo_de_procesos(procesos, repositorios):
 	while len(procesos) < CANTIDAD_DE_PROCESOS_EN_PARALELO:
@@ -25,6 +27,7 @@ def obtener_grupo_de_procesos(procesos, repositorios):
 		else:
 			return
 
+"""
 def obtener_progreso(cantidad, total):
 	width = 6
 	progress = cantidad / total
@@ -40,6 +43,17 @@ def obtener_progreso(cantidad, total):
 		part_char = ""
 	line = "" + "█" * whole_width + part_char + " " * (width - whole_width - 1) + "▏"
 	return line
+"""
+
+def obtener_progreso(cantidad, total):
+	progress = cantidad / total
+	ancho = 15
+	cantidad_de_caracteres_para_el_progreso = math.floor(ancho * progress)
+
+	barra_de_progreso = ("▓" * cantidad_de_caracteres_para_el_progreso)
+	barra_restante = ("░" * (ancho -cantidad_de_caracteres_para_el_progreso))
+
+	return  f"{verde}|{barra_de_progreso}{gris}{barra_restante}{verde}|{fin}"
 
 
 def main():
@@ -56,90 +70,96 @@ def main():
 		filtro = None
 		mensaje = f"Sincronizando repositorios"
 
-	with yaspin(Spinners.arc, text=mensaje + "...", color="blue") as spinner:
-		procesos = []
-		return_codes = []
+	procesos = []
+	return_codes = []
 
-		total = len(repositorios)
-		repositorios_originales = repositorios[:]
+	total = len(repositorios)
+	repositorios_originales = repositorios[:]
 
-		while repositorios:
-			obtener_grupo_de_procesos(procesos, repositorios)
+	while repositorios:
+		obtener_grupo_de_procesos(procesos, repositorios)
 
-			# mientras existan procesos en ejecución:
-			while len(procesos) > 0:
-				for proceso_en_ejecucion in procesos[:]:
+		# mientras existan procesos en ejecución:
+		while len(procesos) > 0:
+			for proceso_en_ejecucion in procesos[:]:
 
-					if proceso_en_ejecucion.poll() is not None:
-						# Si un proceso finaliza
-						return_codes.append(proceso_en_ejecucion.wait())
-						procesos.remove(proceso_en_ejecucion)
-						# Intenta volver a cargar el pool de procesos
-						obtener_grupo_de_procesos(procesos, repositorios)
-						progreso = obtener_progreso(len(return_codes), total)
-						spinner.text = f"{mensaje}: {progreso}"
-						continue
-					else:
-						# Si el comando sigue ejecutando, espera para ir al siguiente proceso en ejecución.
-						progreso = obtener_progreso(len(return_codes), total)
-						spinner.text = f"{mensaje}: {progreso}"
-						time.sleep(.1)
-
-
-		for (indice, x) in enumerate(repositorios_originales):
-			nombre = utilidades.obtener_nombre(x)
-			descripcion_tag = '-'
-
-			if len(nombre) > LIMITE:
-				nombre_corto = nombre[:LIMITE-3] + "..."
-			else:
-				nombre_corto = nombre
-
-			try:
-				branch = utilidades.branch(x)
-			except subprocess.CalledProcessError as error:
-				error = f'{Fore.RED}⊗ error{Style.RESET_ALL}'
-				items.append([nombre_corto, error, error, error, error])
-				continue
-
-
-			try:
-				if return_codes[indice] > 0:
-					estado_remoto = f'{Fore.RED}⊗ error{Style.RESET_ALL}'
-					estado_local = f'{Fore.RED}⊗ error{Style.RESET_ALL}'
+				if proceso_en_ejecucion.poll() is not None:
+					# Si un proceso finaliza
+					return_codes.append(proceso_en_ejecucion.wait())
+					procesos.remove(proceso_en_ejecucion)
+					# Intenta volver a cargar el pool de procesos
+					obtener_grupo_de_procesos(procesos, repositorios)
+					progreso = obtener_progreso(len(return_codes), total)
+					#spinner.text = f"{mensaje}: {progreso}"
+					continue
 				else:
-					cambios = utilidades.cantidad_de_cambios_remotos_no_sincronizados(x, branch)
-					cambios_sin_commits = utilidades.obtener_cambios_sin_commits(x)
-
-					if cambios > 0:
-						estado_remoto = f'{Fore.YELLOW}✓ se sincronizó {Style.RESET_ALL}'
-						utilidades.realizar_pull_y_push(x, branch)
-					else:
-						estado_remoto = f'{Fore.GREEN}✓ sync{Style.RESET_ALL}'
-
-					try:
-						tag = utilidades.obtener_ultimo_tag(x)
-						cantidad = utilidades.obtener_commits_desde_el_tag(x, tag)
-
-						if cantidad > 0:
-							descripcion_tag = f'{Fore.YELLOW}' + "{} - {} ↺".format(tag, cantidad) + f'{Style.RESET_ALL}'
-						else:
-							descripcion_tag = f'{Fore.GREEN}' + '{} ✓'.format(tag) + f'{Style.RESET_ALL}'
-
-					except subprocess.CalledProcessError:
-						tag = ""
+					# Si el comando sigue ejecutando, espera para ir al siguiente proceso en ejecución.
+					progreso = obtener_progreso(len(return_codes), total)
+					#spinner.text = f"{mensaje}: {progreso}"
+					time.sleep(.1)
 
 
-					if cambios_sin_commits > 0:
-						estado_local = f'{Fore.RED}↺ no sync{Style.RESET_ALL}'
-					else:
-						estado_local = f'{Fore.GREEN}✓ sync{Style.RESET_ALL}'
+				print(f"\rSincronizando repositorios: {progreso}", end="")
 
-			except subprocess.CalledProcessError:
+	progreso = obtener_progreso(10, 10)
+	print(f"\r{mensaje}: {verde} {progreso} {fin}", end="")
+	print("\n")
+
+
+	for (indice, x) in enumerate(repositorios_originales):
+		nombre = utilidades.obtener_nombre(x)
+		descripcion_tag = '-'
+
+		if len(nombre) > LIMITE:
+			nombre_corto = nombre[:LIMITE-3] + "..."
+		else:
+			nombre_corto = nombre
+
+		try:
+			branch = utilidades.branch(x)
+		except subprocess.CalledProcessError as error:
+			error = f'{Fore.RED}⊗ error{Style.RESET_ALL}'
+			items.append([nombre_corto, error, error, error, error])
+			continue
+
+
+		try:
+			if return_codes[indice] > 0:
 				estado_remoto = f'{Fore.RED}⊗ error{Style.RESET_ALL}'
 				estado_local = f'{Fore.RED}⊗ error{Style.RESET_ALL}'
+			else:
+				cambios = utilidades.cantidad_de_cambios_remotos_no_sincronizados(x, branch)
+				cambios_sin_commits = utilidades.obtener_cambios_sin_commits(x)
 
-			items.append([nombre_corto, estado_remoto, estado_local, branch, descripcion_tag])
+				if cambios > 0:
+					estado_remoto = f'{Fore.YELLOW}✓ se sincronizó {Style.RESET_ALL}'
+					utilidades.realizar_pull_y_push(x, branch)
+				else:
+					estado_remoto = f'{Fore.GREEN}✓ sync{Style.RESET_ALL}'
+
+				try:
+					tag = utilidades.obtener_ultimo_tag(x)
+					cantidad = utilidades.obtener_commits_desde_el_tag(x, tag)
+
+					if cantidad > 0:
+						descripcion_tag = f'{Fore.YELLOW}' + "{} - {} ↺".format(tag, cantidad) + f'{Style.RESET_ALL}'
+					else:
+						descripcion_tag = f'{Fore.GREEN}' + '{} ✓'.format(tag) + f'{Style.RESET_ALL}'
+
+				except subprocess.CalledProcessError:
+					tag = ""
+
+
+				if cambios_sin_commits > 0:
+					estado_local = f'{Fore.RED}↺ no sync{Style.RESET_ALL}'
+				else:
+					estado_local = f'{Fore.GREEN}✓ sync{Style.RESET_ALL}'
+
+		except subprocess.CalledProcessError:
+			estado_remoto = f'{Fore.RED}⊗ error{Style.RESET_ALL}'
+			estado_local = f'{Fore.RED}⊗ error{Style.RESET_ALL}'
+
+		items.append([nombre_corto, estado_remoto, estado_local, branch, descripcion_tag])
 
 
 	print(tabulate(items, headers=['Repositorio', 'Remoto', 'Local', 'Branch', 'Último tag']))
